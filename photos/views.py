6a5f6c9ff88be31df811c0b764293d753debe9c4
kevin-kili <kilikevin435@gmail.com>
2020-05-24 08:48:52 +0300

    added bootstrap

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 
@@ -21,25 +21,18 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def past_days_news(request,past_date):
+def past_days_news(request, past_date):
 
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
 
-        # Converts data from the string Url
-    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    if date == dt.date.today():
+        return redirect(news_of_day)
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-photos/past-photos.html', {"date": date})
