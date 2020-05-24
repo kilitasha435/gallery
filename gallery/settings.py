3a4b6e221d1557bb832be13198a701471facad32
kevin-kili <kilikevin435@gmail.com>
2020-05-24 23:05:20 +0300

    added some files

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'photos.app.NewsConfig',
     'bootstrap3',
+    'photos.app.PhotosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
