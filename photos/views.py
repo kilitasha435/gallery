e3e77eb787750b33a69ad326936d6d09b631f51b
kevin-kili <kilikevin435@gmail.com>
2020-05-24 08:13:13 +0300

    added new templates

--- photos/views.py
+++ photos/views.py
@@ -4,21 +4,11 @@ import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to my gallery')
+    return render(request, 'welcome.html')
 
 def news_of_day(request):
     date = dt.date.today()
-
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-photos/today-photos.html', {"date": date,})
 
 def convert_dates(dates):
 
