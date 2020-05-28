2c2705bf11735a081f9974cb2c281bc7a8b249b3
kevin-kili <kilikevin435@gmail.com>
2020-05-28 10:27:34 +0300

    added images

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'photos.apps.PhotosConfig',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
+    'photos',
 ]
 
 MIDDLEWARE = [
